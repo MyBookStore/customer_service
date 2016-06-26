365bc2498659c82299ba52a941db52dea47c984d
Akshay <kumarsharmaa69@gmail.com>
2016-06-26 10:22:31 +0530

    Rendering rabl from user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,25 @@
 class UsersController < ApplicationController
   def create
+    delete_if_user_exist
     @user = User.new(user_params)
     if @user.save!
-      render json: @user, status: 201
+      render :show, status: 201
     else
       render nothing: true, status: 422
+    end
+  end
+
+  def delete_if_user_exist
+    existing_user = User.find_by(params[:user][:id])
+    if existing_user
+      User.destroy(existing_user)
     end
   end
 
   def search
     @user = User.find_by(params[:user][:id])
     if @user
-      render json: @user, status: 200
+      render :show, status: 200
     else
       render nothing: true, status: 404
     end
@@ -20,6 +28,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.permit(:name, :email, :password, :password_confirmation)
   end
 end
